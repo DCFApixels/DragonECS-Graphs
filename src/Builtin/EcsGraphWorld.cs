c19cb8cdc70d9fc070dd8094343783e18bdadf1d
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2024-12-04 17:18:18 +0800

    Update EcsGraphWorld.cs

--- src/Builtin/EcsGraphWorld.cs
+++ src/Builtin/EcsGraphWorld.cs
@@ -10,8 +10,9 @@ namespace DCFApixels.DragonECS
     [MetaID("ECC4CF479301897718600925B00A7DB4")]
     public sealed class EcsGraphWorld : EcsWorld, IInjectionUnit
     {
-        public EcsGraphWorld(EcsWorldConfig config, short worldID = -1) : base(config, worldID) { }
-        public EcsGraphWorld(IConfigContainer configs = null, short worldID = -1) : base(configs, worldID) { }
+        public EcsGraphWorld() : base() { }
+        public EcsGraphWorld(EcsWorldConfig config = null, string name = null, short worldID = -1) : base(config, name == null ? "Default" : name, worldID) { }
+        public EcsGraphWorld(IConfigContainer configs, string name = null, short worldID = -1) : base(configs, name == null ? "Default" : name, worldID) { }
         void IInjectionUnit.InitInjectionNode(InjectionGraph nodes) { nodes.AddNode(this); }
     }
 }
