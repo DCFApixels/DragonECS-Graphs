aff9c033d1fead6b49c36b163a777ada191f6892
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-03-19 16:30:51 +0800

    update MetaIDs

--- src/Builtin/EcsGraphWorld.cs
+++ src/Builtin/EcsGraphWorld.cs
@@ -7,7 +7,7 @@ namespace DCFApixels.DragonECS
     [MetaGroup(EcsConsts.PACK_GROUP, EcsConsts.WORLDS_GROUP)]
     [MetaDescription(EcsConsts.AUTHOR, "Inherits EcsWorld without extending its functionality and is used for specific injections. Can be used as argument to EcsWorld.CreateGraph(new " + nameof(EcsGraphWorld) + "()) and to store relation entity.")]
     [DebuggerTypeProxy(typeof(DebuggerProxy))]
-    [MetaID("ECC4CF479301897718600925B00A7DB4")]
+    [MetaID("DragonECS_ECC4CF479301897718600925B00A7DB4")]
     public sealed class EcsGraphWorld : EcsWorld, IInjectionUnit, IInjectionBlock
     {
         public EcsGraphWorld() : base() { }
